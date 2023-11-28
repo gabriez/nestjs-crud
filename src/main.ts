f5a0d2fd0f1dcdd79f055601ee11cfb5c537306f
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-28 13:57:33 -0400

    uploading cors permissions

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:'http://localhost:3000',
+    origin:['http://localhost:3000', 'https://nextjs-crud-pied-five.vercel.app'],
     credentials: true
     });
   await app.listen(3001);
