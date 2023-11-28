61ac6adcc145d1dc6aa13b92b12418a77d3f9e4d
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-28 17:31:22 -0400

    cors allow

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['http://localhost:3000', 'https://nextjs-crud-pied-five.vercel.app'],
+    origin:['http://localhost:3000', 'https://nextjs-crud-6tis5lr9a-gabriez.vercel.app/'],
     credentials: true
     });
   await app.listen(process.env.PORT || 3001);
